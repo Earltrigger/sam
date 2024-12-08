f6cde03a32de907ac60ad603db57da47d83b051a
Karennewton12 <newtonkaren782@gmail.com>
2024-12-08 06:12:30 -0600

    nm

---
+++ script.js
@@ -0,0 +1,39 @@
+let myName = "James"
+let myAge = 16
+let myState = "Ebonyi"
+console.log (" my name is" + myName + ", i am " + myAge + ", and i am from " + myState );
+console.log (`My name is ${myName} i am from ${myState} i am ${myAge} years old`)
+
+let name = "kelvin"
+let age = 20
+
+if (age >= 20) { 
+    console.log(`${name} your are not Elegible`);
+}
+else{
+    console.log(`${name} your are Elegible`);    
+}
+
+let Name = "Uchenna"
+let Age = 20
+
+if (age >=20) {
+    console.log ( " you are not Elegible " + Name );
+}
+else{
+    console.log( " you are Elegible " + Name );
+}
+
+let myVariable = "Hello, World"
+let myNames = "Kelvin Bryan"
+let Variable = "Web Developer"
+let State = "Rivers"
+
+console.log ( myVariable + " My name is " + myNames + " i am a " + Variable + " and i am from " + State )
+
+let myVariables = "Hello, World"
+let myNamess = "Uche Briggs"
+let Variablee = "Realator"
+let Statee = "Delta State"
+
+console.log(` ${myVariables}  My name is  ${myNamess}  i am a  ${Variablee}  and i am from  ${Statee}`)
